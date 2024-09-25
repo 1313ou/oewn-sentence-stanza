fbdb6cdbd4b82b97243276219c170f994ccfccb3
1313ou <1313ou@gmail.com>
2024-09-25 10:36:35 +0200

    wip

--- sentence.py
+++ sentence.py
@@ -18,26 +18,52 @@ def _dump(sentence):
     print(sconstituency)
 
 
-def _is_sentence2(sentence):
-    # _dump(sentence)
-    roots = [d[2] for d in sentence.dependencies if d[1] in ('root')]
-    if len(roots) != 1:
-        return False
-    root = roots[0]
+def find_dep1(rels, head, dependencies):
+    return next(find_dep_gen(rels, head, dependencies), None)
 
-    # root is a verb or aux
+
+def find_dep_gen(rels, head, dependencies):
+    return (d[2] for d in dependencies if d[1] in rels and d[2].head == head)
+
+
+def find_dep_list(rels, head, dependencies):
+    # return [d[2] for d in dependencies if d[1] in rels and d[2].head == head]
+    return list(find_dep_gen(rels, head, dependencies))
+
+
+def __is_sentence(sentence):
+    # _dump(sentence)
+    root = find_dep1(('root'), 0, sentence.dependencies)
+    if not root:
+        return False
+
+    # root is a verb
     if root.upos in {"VERB"}:
+        # let
+        if root.lemma == 'let' and root.id == 1:
+            # let's|us|him|the man V
+            xcomp = find_dep1(('xcomp'), root.id, sentence.dependencies)
+            obj = find_dep1(('obj'), root.id, sentence.dependencies)
+            if xcomp and xcomp.upos == 'VERB' and obj and obj.upos in ('NOUN', 'PRON'):
+                return True
+            ccomp = find_dep1(('ccomp'), root.id, sentence.dependencies)
+            if ccomp:
+                return True
+
+        # do
+        aux = find_dep1(('aux'), root.id, sentence.dependencies)
+        if aux and aux.lemma == 'do' and aux.id == 1:
+            return True
+
         # Check for nominal or clausal subject linked to the root
-        subjects = [d[2] for d in sentence.dependencies if
-                    d[1] in ("nsubj", "nsubj:pass", "nsubjpass", "csubj") and d[2].head == root.id]
+        subjects = find_dep_list(('nsubj', 'nsubj:pass', 'nsubjpass', 'csubj'), root.id, sentence.dependencies)
         if len(subjects) == 0:
             return False
-        subject = subjects[0]
         return True
 
+    # root is a noun
     elif root.upos in {"NOUN", "PRON", "ADJ"}:
-        copulas = [d[2] for d in sentence.dependencies if
-                   d[1] in ("cop") and d[2].head == root.id]
+        copulas = find_dep_list(("cop"), root.id, sentence.dependencies)
         if len(copulas) == 0:
             return False
         return True
@@ -46,7 +72,7 @@ def _is_sentence2(sentence):
 
 
 def _is_sentence(doc):
-    return len(doc.sentences) == 1 and _is_sentence2(doc.sentences[0])
+    return len(doc.sentences) == 1 and __is_sentence(doc.sentences[0])
 
 
 # P R O C E S S I N G   F U N C T I O N   F R O M   T E X T
@@ -82,28 +108,33 @@ def main():
     examples1 = [
         "go",
         "don't go",
+        "do go",
         "let's go",
+        "let me explain",
+        "let the man go",
+        "let there be more light",
     ]
     examples2 = [
         "is anybody here",
-        "A full thought, it is.",
-
         "this is obvious",
-        "Incomplete",
-        "running fast",
         "This is a sentence.",
         "I like music",
         "This is a complete sentence.",
-        "What about this?",
         "The quick brown fox jumps over the lazy dog.",
-        "a quick brown fox",
         "She loves programming and solving complex problems.",
         "The cat sat on the mat.",
         "He was smoking.",
-        "obvious though this is ",
         "do you smoke",
     ]
-    for input_text in examples1:  # + examples2:
+    examples3 = [
+        "A full thought, it is.",
+        "Incomplete",
+        "running fast",
+        "What about this?",
+        "a quick brown fox",
+        "obvious though this is ",
+    ]
+    for input_text in examples1 + examples2 + examples3:
         doc = nlp(input_text)
         sentence_result = _is_sentence(doc)
 
